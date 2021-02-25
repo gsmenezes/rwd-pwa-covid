18e0ca7ed821bda3b1f5cd24f21de9cfe3005c37
gimenezes <menezes.s.gilda@gmail.com>
2021-02-25 12:22:53 -0300

    add country mock and change image names

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,35 @@
+import Brazil from '../../assets/images/brazil-flag.png';
+import China from '../../assets/images/china-flag.png';
+import Germany from '../../assets/images/germany-flag.png';
+import Italy from '../../assets/images/italy-flag.png';
+import Usa from '../../assets/images/usa-flag.png';
+
+const COUNTRIES = [
+  {
+    label: 'Alemanha',
+    value:'germany',
+    flag: Germany
+  },
+  {
+    label: 'Brasil',
+    value:'brazil',
+    flag: Brazil
+  },
+  {
+    label: 'China',
+    value:'china',
+    flag: China
+  },
+  {
+    label: 'Estados Unidos',
+    value:'usa',
+    flag: Usa
+  },
+  {
+    label: 'Itália',
+    value:'italy',
+    flag: Italy
+  },
+]
+
+export default COUNTRIES;
